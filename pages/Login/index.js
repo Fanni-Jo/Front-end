3a2a1d4f6f6d58a92a516e7bd67a9bdd7ec00039
Barham Farraj <barhamfarraj@icloud.com>
2022-07-03 14:37:34 +0300

    login

--- pages/Login/index.js
+++ pages/Login/index.js
@@ -1,7 +1,17 @@
 import Link from 'next/link'
-
+import axios from "axios"
 
 function Login() {
+  
+  const logIn = async (event) => {  
+    event.preventDefault();
+    await axios.post('https://fanni-jo.herokuapp.com/login', {
+        username: event.target.username.value,
+        password: event.target.password.value
+    }).then(res => {
+      console.log(res)
+    });
+    }
   return (
     <>
 
@@ -12,24 +22,24 @@ function Login() {
 
           <div className="card-body p-5 text-center">
 
-            <form className="mb-md-5 mt-md-1 pb-5 login-form">
+            <form className="mb-md-5 mt-md-1 pb-5 login-form" onSubmit={()=>{logIn()}} >
 
               <h2 className="fw-bold mb-2 text-uppercase ">Login</h2>
               <p className="text-white-50 mb-5">Please enter your E-mail and Password!</p>
 
               <div className="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" className="form-control form-control-lg" />
-                <label className="form-label" for="typeEmailX">Email</label>
+                <input type="username" id="username" className="form-control form-control-lg" />
+                <label className="form-label" for="typeEmailX">Username</label>
               </div>
 
               <div className="form-outline form-white mb-4">
-                <input type="password" id="typePasswordX" className="form-control form-control-lg" />
+                <input type="password" id="password" className="form-control form-control-lg" />
                 <label className="form-label" for="typePasswordX">Password</label>
               </div>
 
               <p className="small  pb-lg-2"><Link  href="/forget"><a className="text-white-50" href="#!">Forgot password?</a></Link></p>
 
-              <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+              <button className="btn btn-outline-light btn-lg px-5" type="submit" >Login</button>
 
               <div className="d-flex justify-content-center text-center mt-4 pt-1">
                 <a href="#!" className="text-white"><i className="fab fa-facebook-f fa-lg"></i></a>
@@ -89,6 +99,7 @@ function Login() {
     
 </>
   )
+  
 }
 
 export default Login
--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,8 +1,13 @@
 import Form from "./form"
+import { useState } from "react"
+
 
 export default function SignUp(){
+    
+    const{userType,setUserType} = useState(null);
+    console.log(userType)
     return(
-        <Form />
-
+        
+    <Form/>
     )
 }
